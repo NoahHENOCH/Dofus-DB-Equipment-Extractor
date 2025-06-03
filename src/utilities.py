f546dc623adc4216d0d2bbd669a59e7ac8b5abef
NoahHENOCH <henoch.noah@gmail.com>
2025-06-04 00:06:17 +0200

    Correction d'une erreur avec le import time et le from time import time

--- src/utilities.py
+++ src/utilities.py
@@ -72,9 +72,9 @@ def time_to_execute(func: Callable[..., Any], *args: Any, **kwargs: Any) -> Any:
     Returns:
         Any: Le résultat de la fonction exécutée.
     """
-    start_time = time.time()
+    start_time = time()
     result = func(*args, **kwargs)
-    end_time = time.time()
+    end_time = time()
     execution_time = end_time - start_time
     print(f"Execution time: {execution_time:.2f} seconds")
     return result
